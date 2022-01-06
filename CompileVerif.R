8b51125b52a2ebf87ad8fb71257add0a2069ecf0
Christophe Dutang <dutangc@gmail.com>
2022-01-06 16:14:26 +0100

    small R code to check

---
+++ CompileVerif.R
@@ -0,0 +1,8 @@
+library("ctv")
+ctv2html("ExtremeValue.md")
+browseURL("ExtremeValue.html")
+
+#see help at https://github.com/cran-task-views/ctv/blob/main/Contributing.md
+
+check_ctv_packages("ExtremeValue.md")
+
